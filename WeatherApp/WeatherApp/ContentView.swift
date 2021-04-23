f0dfec1fc29a7577fda5c561fa2f1dd53be7e289
Abayevtamerlan123 <tamerlan.00.04@mail.ru>
2021-04-24 03:28:49 +0500

    swiftUI

---
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -0,0 +1,106 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Тамирлан Абаев   on 23.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var isChange = false
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors:  [isChange ? .blue : .black, isChange ? Color("lightBlue"): .gray ]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Cupertino,Ca")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing: 10) {
+                    
+                    Image(systemName: isChange ? "cloud.sun.fill" : "moon.stars.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                
+                .padding(.bottom,40)
+                
+                HStack(spacing:20) {
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 74)
+
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "sun.max.fill",
+                                   temperature: 88)
+
+                    WeatherDayView(dayOfWeek: "THU",
+                                   imageName: "wind.snow",
+                                   temperature: 55)
+
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "sunset.fill",
+                                   temperature: 60)
+
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "snow",
+                                   temperature: 25)
+                }
+                Spacer()
+                
+                Button {
+                    isChange.toggle()
+                } label: {
+                    Text("Change Day Time")
+                        .frame(width: 280, height: 50)
+                        .background(Color.white)
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .cornerRadius(10)
+                }
+                
+                Spacer()
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            
+            Image(systemName: imageName)
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 40, height: 40)
+            
+            Text("\(temperature)°")
+            .font(.system(size: 28, weight: .medium))
+            .foregroundColor(.white)
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Тамирлан Абаев   on 23.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
